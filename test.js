2c685d0bf5f8b5ac77100380b149c99bec4d54a0
wyf <wei.yinfu@qq.com>
2018-12-30 01:17:59 +0800

    update github

--- test.js
+++ test.js
@@ -1,13 +1,5 @@
-var p = "http://localhost:8899/crawling.html"
-var url = require("url")
-var res = url.parse(p)
-console.log(url)
-console.log(res)
-x = new url.Url()
-x.host = res.host
-x.port = res.port
-x.protocol = "ws"
-x.pathname = "/crawling"
-x.search = "username=haha"
-console.log(x)
-console.log(url.format(x))
+var mongo = require("mongodb")
+mongo.connect("mongodb://weiyinfu.cn:27017", {}, (err, cli) => {
+    console.log(err)
+    console.log(cli)
+})
